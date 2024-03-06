c5cd7251d105559cf16bb4eba401bd32395435c3
ShardOfDebil <pavlushok38@yandex.ru>
2024-03-06 16:00:51 +0300

    feat(card) - added cache

--- src/shared/services/data.service.ts
+++ src/shared/services/data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {Observable, shareReplay} from 'rxjs';
 import { map } from 'rxjs/operators';
 import {IGameCard} from '../../core/interfaces/data.interface';
 
@@ -8,14 +8,17 @@ import {IGameCard} from '../../core/interfaces/data.interface';
   providedIn: 'root'
 })
 export class DataService {
-
+  private cache$: Observable<IGameCard[]> | undefined;
   constructor(private http: HttpClient) { }
 
   private getGameCards(): Observable<IGameCard[]> {
-    return this.http.get<{gameCards: IGameCard[]}>('assets/data/sitedb.json')
-      .pipe(
-        map((response: {gameCards: IGameCard[]}) => response.gameCards)
+    if (!this.cache$) {
+      this.cache$ = this.http.get<{gameCards: IGameCard[]}>('assets/data/sitedb.json').pipe(
+        map((response: {gameCards: IGameCard[]}) => response.gameCards),
+        shareReplay(1)
       );
+    }
+    return this.cache$;
   }
 
   public getGameCardsPublic(): Observable<IGameCard[]> {
